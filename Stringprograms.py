8fd46a6c737db7a8b4da778aa0b1ef370daee293
jyothsna <jyothsnareddybk@gmail.com>
2019-09-10 16:42:52 +0530

    python programs

---
+++ Stringprograms.py
@@ -0,0 +1,12 @@
+s="pythan for life"
+print(s[0:6])
+print(s[7:10])
+print(s[11:15])
+print(s[2:6])
+print(s[2:10])
+print(s[8:13])
+print(s[0:6].lower(), s[7:10].lower(), s[11:16].upper())
+print(s[0:6].upper(), s[7:10].lower(), s[11:16].upper())
+print(s[::2])
+# print(s.count("p"))
+
